5a946416245d66f46b504571f3ac2c33234b0162
pierre-antoine.fournier <paf.fournier@gmail.com>
2025-05-19 16:20:55 +0800

    add shader util

---
+++ shader/shader.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <glad/glad.h>
+
+GLuint compile_shader(GLenum shader_type, char* src) {
+    GLuint shader = glCreateShader(shader_type);
+    glCompileShader(shader);
+    GLint success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char log[512];
+        glGetShaderInfoLog(shader, sizeof(log), NULL, log);
+        fprintf(stderr, "Error shader %s compile: \n%s\n",
+                (shader_type == GL_VERTEX_SHADER ? "vertex" :
+                 shader_type == GL_FRAGMENT_SHADER ? "fragment" : "unknow"),
+                log);
+        glDeleteShader(shader);
+        return -1;
+    }
+    return shader;
+}
+
+GLuint create_shader(char* vertex, char* fragment) {
+    GLuint shader_v = compile_shader(GL_VERTEX_SHADER, vertex);
+    if (!shader_v) {
+        return -1;
+    }
+    GLuint shader_f = compile_shader(GL_FRAGMENT_SHADER, fragment);
+    if (!shader_f) {
+        glDeleteShader(shader_v);
+        return -1;
+    }
+    GLuint program = glCreateProgram();
+    glAttachShader(program, shader_v);
+    glAttachShader(program, shader_f);
+    glLinkProgram(program);
+    GLint linked;
+    glGetProgramiv(program, GL_LINK_STATUS, &linked);
+    if (!linked) {
+        char log[512];
+        glGetProgramInfoLog(program, sizeof(log), NULL, log);
+        fprintf(stderr, "Error link: \n%s\n", log);
+        glDeleteProgram(program);
+    }
+    glDeleteShader(shader_v);
+    glDeleteShader(shader_f);
+    return program;
+}
---
+++ shader/shader.h
@@ -0,0 +1,8 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+GLuint create_shader(char* vertex, char* fragment);
+
+#endif
